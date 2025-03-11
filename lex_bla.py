0afce3ed4a6c653b702abe862f9067c6389fa120
Lutho <luthoyrn@gmail.com>
2025-03-11 18:14:27 +0200

    Added multi-line comment support

--- lex_bla.py
+++ lex_bla.py
@@ -2,60 +2,55 @@ import ply.lex as lex
 import sys
 
 # Define token names
-tokens = ['COMMENT','OP','ID', 'BINARY_LITERAL','WHITESPACE']
+tokens = ['COMMENT', 'OP', 'ID', 'BINARY_LITERAL', 'WHITESPACE']
 
-# Token 
-def t_COMMENT(t):
-    r'//.*' 
-    return t 
-
-def t_WHITESPACE(t):
-    r'\s'
-    return t 
+t_OP = r'[ASMD=()]'
 
 def t_ID(t):
-    r'[a-z_][a-zA-Z0-9_]*' 
+    r'[a-z_][a-z0-9_]*'
     return t
 
 def t_BINARY_LITERAL(t):
-    r'[+-]?[01]+'  
+    r'[+-]?[01]+'
     return t
 
-def t_OP(t):
-    r'[ASMD=()]'
+def t_COMMENT(t):
+    r'//.*|/\*[\s\S]*?\*/'
     return t
 
-def t_error(t): 
-    print(f"Illegal character '{t.value[0]}'")
+def t_WHITESPACE(t):
+    r'\s+'
+    return t
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
     t.lexer.skip(1)
 
-# Building the lexer
 lexer = lex.lex()
 
 def process_file(input_filename):
     output_filename = input_filename.replace(".bla", ".tkn")
     try:
         with open(input_filename, 'r', encoding='utf-8') as file_in, open(output_filename, 'w', encoding='utf-8') as file_out:
-            for line in file_in:
-                lexer.input(line)
-                while True:
-                    tok = lexer.token()
-                    if not tok:
-                        break
-                    if tok.type=="OP":
-                        token_output = f"{tok.value}"
-                    elif tok.type =="WHITESPACE":
-                        token_output = f"{tok.type}"
-                    elif tok.type == 'COMMENT':
-                        token_output = f"{tok.type}"
-                    else:
-                        token_output = f"{tok.type},{tok.value}"
-                    file_out.write(token_output + "\n") 
+            content = file_in.read()
+            lexer.input(content)
+            while True:
+                tok = lexer.token()
+                if not tok:
+                    break
+                if tok.type=="OP":
+                    token_output = f"{tok.value}"
+                elif tok.type in ["WHITESPACE","COMMENT"]:
+                    token_output = f"{tok.type}"
+                else:
+                    token_output = f"{tok.type},{tok.value}"          
+                file_out.write(token_output + "\n") 
         print(f"Tokens saved to {output_filename}")
     except FileNotFoundError:
         print(f"Error: File '{input_filename}' not found!")
 
-if len(sys.argv) > 1:
-    process_file(sys.argv[1])
-else:
-    print('Usage: python lex_bla.py <filename>.bla')
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        process_file(sys.argv[1])
+    else:
+        print('Usage: python lex_bla.py <filename>.bla')

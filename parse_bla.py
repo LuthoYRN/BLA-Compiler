473aa44e0b6e9a336ee2c77492c56dcb9fd0d139
Lutho <luthoyrn@gmail.com>
2025-03-11 21:21:52 +0200

    Implemented writing to ast file

--- parse_bla.py
+++ parse_bla.py
@@ -1,8 +1,8 @@
 import ply.yacc as yacc
-from lex_bla import tokens,lexer
+from lex_bla import tokens, lexer
 import sys
 
-#Parsing rules
+# Parsing rules
 def p_program(p):
     '''
     program : program statement
@@ -69,6 +69,43 @@ def p_error(p):
 # Building the parser
 parser = yacc.yacc()
 
+def generate_ast_string(node, indent=1):
+    """
+    Recursively generate AST string representation with proper indentation
+    """
+    if not isinstance(node, tuple):
+        # Terminal node
+        if isinstance(node, str) and node.startswith(('0', '1', '+', '-')) or node.isdigit() or (node.startswith(('+', '-')) and node[1:].isdigit()):
+            return '\t' * indent + f"BINARY_LITERAL,{node}"
+        elif isinstance(node, str):
+            return '\t' * indent + f"ID,{node}"
+        return '\t' * indent + str(node)
+    
+    # Non-terminal node
+    op, left, right = node
+    result = '\t' * indent + op + '\n'
+    
+    # left child
+    result += generate_ast_string(left, indent + 1) + '\n'
+    
+    # right child
+    result += generate_ast_string(right, indent + 1)
+    
+    return result
+
+def write_ast_file(ast, filename):
+    output_filename = filename.replace(".bla", ".ast")
+    
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        f.write("Program\n")
+        
+        for i, statement in enumerate(ast):
+            stmt_str = generate_ast_string(statement)
+            f.write(stmt_str)
+            f.write('\n')
+    
+    print(f"AST saved to {output_filename}")
+
 def process_file(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         data = f.read()
@@ -76,8 +113,9 @@ def process_file(filename):
     lexer.input(data) 
     result = parser.parse(lexer=lexer)  # Parse tokens
 
-    # Output AST
     print("AST:", result)
+    
+    write_ast_file(result, filename)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
